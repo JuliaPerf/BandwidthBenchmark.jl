a0c223588d756fd64b8ae955c74eb1a754de1814
Carsten Bauer <crstnbr@gmail.com>
2021-10-12 21:50:14 +0200

    scaling update

--- benchmark/scaling/scaling.jl
+++ benchmark/scaling/scaling.jl
@@ -1,15 +1,3 @@
 using BandwidthBenchmark
-using BandwidthBenchmark.PrettyTables
 
-# we take NT as the upper limit for our scaling analysis
-NT = Threads.nthreads()
-sdaxpy_results = zeros(NT)
-for n in 1:NT
-    df = bwbench(; nthreads=n, verbose=true)
-    sdaxpy_results[n] = last(df.var"Rate (MB/s)")
-    flush(stdout) # to be safe
-end
-
-# print results
-println("\n\nScaling results:")
-pretty_table(hcat(1:NT, sdaxpy_results); header=["# Threads", "SDaxpy Bandwidth (MB/s)"])
+bwscaling()
