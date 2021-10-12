0f82572b6e409443188acbf2729a9748b720ba1d
Carsten Bauer <crstnbr@gmail.com>
2021-10-12 16:08:42 +0200

    likwid2

--- benchmark/likwid/run_bwbench_likwid.sh
+++ benchmark/likwid/run_bwbench_likwid.sh
@@ -1,2 +1,2 @@
 # likwid-perfctr -c 0-7 -g MEM_DP -m likwid-pin -s 0xfffffffffffff801 -c 0-7 julia --project=. --math-mode=fast -t8 bwbench_likwid.jl
-JULIA_EXCLUSIVE=1 likwid-perfctr -c 0-7 -g MEM -m julia --project=. --math-mode=fast -t8 bwbench_likwid.jl
+JULIA_EXCLUSIVE=1 likwid-perfctr -V 3 -c 0-7 -g MEM -m julia --project=. --math-mode=fast -t8 bwbench_likwid.jl
