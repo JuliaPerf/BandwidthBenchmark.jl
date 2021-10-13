6918a7b96549217116fbbea640d3ac16462cc755
Carsten Bauer <crstnbr@gmail.com>
2021-10-13 09:20:31 +0200

    fix

--- benchmark/scaling_flops/scaling_flops.jl
+++ benchmark/scaling_flops/scaling_flops.jl
@@ -8,6 +8,8 @@ Threads.@threads :static for i in 1:Threads.nthreads()
 end
 println()
 
+title = length(ARGS) > 0 ? join(ARGS, " ") : nothing
+
 data = BandwidthBenchmark.flopscaling()
-p = lineplot(data[:,1], data[:,2], title = "Compact Pinning", xlabel = "# cores", ylabel = "MFlops/s", border = :ascii, canvas = AsciiCanvas)
+p = lineplot(data[:,1], data[:,2], title = title, xlabel = "# cores", ylabel = "MFlops/s", border = :ascii, canvas = AsciiCanvas)
 print(stdout, p)
