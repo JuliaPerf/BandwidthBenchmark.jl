02f2404c59ebece1e249e64cad2a5fe50d274644
Carsten Bauer <carsten.bauer@uni-paderborn.de>
2022-03-22 13:07:32 +0100

    noctua2 benchmarks

---
+++ benchmarks/noctua2_pc2/bwbench/bwbench.jl
@@ -0,0 +1,63 @@
+#!/usr/bin/env sh
+#SBATCH -J bwbench
+#SBATCH -N 1
+#SBATCH -n 128
+#SBATCH -A pc2-mitarbeiter
+#SBATCH -p ALL
+#SBATCH -t 01:30:00
+#SBATCH --hint=nomultithread
+#=
+ml load lang
+ml load Julia/1.7.2-linux-x86_64
+export JULIA_NUM_THREADS=128
+julia --project $(scontrol show job $SLURM_JOBID | awk -F= '/Command=/{print $2}')
+exit
+# =#
+
+println("Node: ", gethostname(), "\n")
+
+using ThreadPinning
+using BandwidthBenchmark
+using UnicodePlots
+
+# Compact Pinning
+println("----------------- Compact Pinning -----------------")
+pinthreads(:compact)
+threadinfo(; color=false)
+println("Check compact pinning: ", getcpuids(), "\n")
+
+
+# bwbench(; verbose=true);
+println("bwbench():")
+bwbench(; verbose=true, write_allocate=true);
+
+println("bwscaling():")
+data = bwscaling()
+p = lineplot(data[:, 1], data[:, 2], title="Bandwidth Scaling, Compact Pinning", xlabel="# cores", ylabel="MB/s", border=:ascii, canvas=AsciiCanvas)
+println(p)
+
+println("flopsscaling():")
+data = BandwidthBenchmark.flopsscaling()
+p = lineplot(data[:, 1], data[:, 2], title="Flops Scaling, Compact Pinning", xlabel="# cores", ylabel="MFlops/s", border=:ascii, canvas=AsciiCanvas)
+println(p)
+
+# Scatter Pinning
+println("----------------- Scatter Pinning -----------------")
+pinthreads(:scatter)
+threadinfo(; color=false)
+println("Check scatter pinning: ", getcpuids(), "\n")
+
+# bwbench(; verbose=true);
+println("bwbench():")
+bwbench(; verbose=true, write_allocate=true);
+
+println("bwscaling():")
+data = bwscaling()
+p = lineplot(data[:, 1], data[:, 2], title="Bandwidth Scaling, Scattered Pinning", xlabel="# cores", ylabel="MB/s", border=:ascii, canvas=AsciiCanvas)
+println(p)
+
+println("flopsscaling():")
+data = BandwidthBenchmark.flopsscaling()
+p = lineplot(data[:, 1], data[:, 2], title="Flops Scaling, Scattered Pinning", xlabel="# cores", ylabel="MFlops/s", border=:ascii, canvas=AsciiCanvas)
+println(p)
+
