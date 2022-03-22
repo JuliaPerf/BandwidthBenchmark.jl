975832bef2a3e092de81c6c0a048b3be51639e37
Carsten Bauer <carsten.bauer@uni-paderborn.de>
2022-03-22 13:06:07 +0100

    update noctua benchmarks + gitignore

--- benchmarks/noctua_pc2/bwbench/bwbench.jl
+++ benchmarks/noctua_pc2/bwbench/bwbench.jl
@@ -1,10 +1,29 @@
+#!/usr/bin/env sh
+#SBATCH -J bwbench
+#SBATCH -N 1
+#SBATCH -A pc2-mitarbeiter
+#SBATCH -p all
+#SBATCH -t 01:30:00
+#SBATCH --hint=nomultithread
+#=
+ml load lang/Julia/1.7.2-linux-x86_64
+export JULIA_NUM_THREADS=40
+julia --project $(scontrol show job $SLURM_JOBID | awk -F= '/Command=/{print $2}')
+exit
+# =#
+
+println("Node: ", gethostname(), "\n")
+
 using ThreadPinning
 using BandwidthBenchmark
 using UnicodePlots
 
 # Compact Pinning
+println("----------------- Compact Pinning -----------------")
 pinthreads(:compact)
+threadinfo(; color=false)
 println("Check compact pinning: ", getcpuids(), "\n")
+
 
 # bwbench(; verbose=true);
 println("bwbench():")
@@ -12,16 +31,18 @@ bwbench(; verbose=true, write_allocate=true);
 
 println("bwscaling():")
 data = bwscaling()
-p = lineplot(data[:,1], data[:,2], title = "Bandwidth Scaling, Compact Pinning", xlabel = "# cores", ylabel = "MB/s", border = :ascii, canvas = AsciiCanvas)
+p = lineplot(data[:, 1], data[:, 2], title="Bandwidth Scaling, Compact Pinning", xlabel="# cores", ylabel="MB/s", border=:ascii, canvas=AsciiCanvas)
 println(p)
 
 println("flopsscaling():")
-data = BandwidthBenchmark.flopscaling()
-p = lineplot(data[:,1], data[:,2], title = "Flops Scaling, Compact Pinning", xlabel = "# cores", ylabel = "MFlops/s", border = :ascii, canvas = AsciiCanvas)
+data = BandwidthBenchmark.flopsscaling()
+p = lineplot(data[:, 1], data[:, 2], title="Flops Scaling, Compact Pinning", xlabel="# cores", ylabel="MFlops/s", border=:ascii, canvas=AsciiCanvas)
 println(p)
 
 # Scatter Pinning
+println("----------------- Scatter Pinning -----------------")
 pinthreads(:scatter)
+threadinfo(; color=false)
 println("Check scatter pinning: ", getcpuids(), "\n")
 
 # bwbench(; verbose=true);
@@ -30,11 +51,11 @@ bwbench(; verbose=true, write_allocate=true);
 
 println("bwscaling():")
 data = bwscaling()
-p = lineplot(data[:,1], data[:,2], title = "Bandwidth Scaling, Scattered Pinning", xlabel = "# cores", ylabel = "MB/s", border = :ascii, canvas = AsciiCanvas)
+p = lineplot(data[:, 1], data[:, 2], title="Bandwidth Scaling, Scattered Pinning", xlabel="# cores", ylabel="MB/s", border=:ascii, canvas=AsciiCanvas)
 println(p)
 
 println("flopsscaling():")
-data = BandwidthBenchmark.flopscaling()
-p = lineplot(data[:,1], data[:,2], title = "Flops Scaling, Scattered Pinning", xlabel = "# cores", ylabel = "MFlops/s", border = :ascii, canvas = AsciiCanvas)
+data = BandwidthBenchmark.flopsscaling()
+p = lineplot(data[:, 1], data[:, 2], title="Flops Scaling, Scattered Pinning", xlabel="# cores", ylabel="MFlops/s", border=:ascii, canvas=AsciiCanvas)
 println(p)
 
